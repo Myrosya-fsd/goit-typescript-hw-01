2666a1f18034cd5bf3bb21a9dc16645360fd9230
myroslava <murosja80@gmail.com>
2025-05-15 18:51:15 +0300

    hw-01

---
+++ src/basic/3.ts
@@ -0,0 +1,10 @@
+// Union type: може бути або рядком, або числом
+let value: string | number;
+// або:
+value = 42;
+value = "Hello";
+
+// Literal type: тільки 'enable' або 'disable'
+let status: "enable" | "disable";
+status = "enable";
+status = "disable";
